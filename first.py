1437c8f21b65ee8b9e654ef9cbe0eedc87c643b1
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-21 11:44:46 +0200

    update

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
+import random
+
 # function 
 def get_choises():
   player_choice = input("enter a choice (rock,paper, scissors)")
-  computer_choice = "paper"
+  options = ["rock", "paper", "scissors"]
+
+  computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
 
   return choices
@@ -14,3 +18,17 @@ print("hi there")
 
 # dictionaries
 dict = {"name":"beau", "color":choices }
+number1 = 25
+number2 = 25
+
+if number1>number2:
+  print(number1)
+elif number2>number1:
+  print(number2)
+else:
+  print('there is no result')
+
+
+  food = ["pizza", "carrots", "eggs"]
+  dinner = random.choice(food)
+  # print(dinner)
