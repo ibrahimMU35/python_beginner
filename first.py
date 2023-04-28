67a1c5f53ff9f743c5c643d45cdc2b9989386a04
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-28 15:02:35 +0200

    update

--- first.py
+++ first.py
@@ -12,8 +12,6 @@ def get_choises():
 
 # function arguments
 def check_win(player, computer):
-  # print("you chose " + player + " ,computer chose " + computer)
-
   print(f"You chose {player}, computer chose {computer}")
   if player == computer:
     return "it's a tie"
@@ -35,43 +33,5 @@ def check_win(player, computer):
       
 choices = get_choises()
       
-  # elif player == "rock" and computer == "scissors":
-  #   return "Rock smashes scissors! You win!"
-  # elif player == "rock" and computer == "paper":
-  #   return "Paper covers rock! You lose!"
-# check_win("rock", "paper")
-# we can use fstring
-
-# age = 25 
-# print(f"jim is {age} years old")
-
-
-
-
-# a = 3
-# b = 5
-# if a == b:
-#   print(yes)
-
-
-# choices = get_choises()
-
-# print(choices)
-# print("hi there")
-
-# # dictionaries
-# dict = {"name":"beau", "color":choices }
-# number1 = 25
-# number2 = 25
-
-# if number1>number2:
-#   print(number1)
-# elif number2>number1:
-#   print(number2)
-# else:
-#   print('there is no result')
-
-
-#   food = ["pizza", "carrots", "eggs"]
-#   dinner = random.choice(food)
-#   # print(dinner)
+choices = {"player": "rock", "computer":"paper"}
+p_choice = choices["player"]
